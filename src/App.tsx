2f924c3c367aea47a30fd1014d24f898d6662e04
Pedro Costa <pedro4costa@outlook.com>
2026-01-19 09:24:10 -0300

    refactor: new version of portifolio with vite + react + tailwind + typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-zinc-900 text-zinc-100">
+      <h1 className="text-4xl font-bold">
+        Meu portfólio 🚀
+      </h1>
+    </main>
+  )
+}
+
+export default App
