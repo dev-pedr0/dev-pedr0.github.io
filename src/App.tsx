9eb632c97fa57860581290879719a2560d78aca0
Pedro Costa <pedro4costa@outlook.com>
2026-01-19 09:54:36 -0300

    Add: css components and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
-
 function App() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-zinc-900 text-zinc-100">
       <h1 className="text-4xl font-bold">
         Meu portfólio 🚀
       </h1>
+      <section className="section-box mt-12 flex flex-col gap-6">
+        <h2 className="text-2xl font-bold uppercase">
+          Projetos
+        </h2>
+      </section>
     </main>
   )
 }
