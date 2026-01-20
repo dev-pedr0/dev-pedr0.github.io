a71db6916ebf7eabf5ae155e53f738f7bc598b4b
Pedro Costa <pedro4costa@outlook.com>
2026-01-20 11:37:14 -0300

    add: certification page

--- src/content/site.ts
+++ src/content/site.ts
@@ -19,3 +19,46 @@ export const AboutContent = {
   text_two: "Venho explorando diversas linguagens e áreas de atuação, mas tenho uma proximidade e afinidade muito grande com tudo front-end. Design e desenvolvimento de sites, páginas, interfaces, telas são meu foco. Busco produzir designs responsivos, intuitivos, simples e harmoniosos garantindo a coesão do todo o projeto. Nesse processo de criação realizo muitas buscas por referencias, e escuto os feedbacks dos clientes e usuários. As vezes mais tempo é levado nesta etapa do que nos momentos de produção de código. Infelizmente não sou um bom artista manual mais acredito que cheguei o mais próximo de entender como um pintor se sente ao terminar uma obra quando finalizo um projeto.",
   text_three: "‘Eu não tenho medo do homem que praticou 10.000 chutes diferentes, mas sim do homem que praticou o mesmo chute 10.000 vezes.’. Apesar de entender o valor da mensagem que o grande lutador Bruce Lee passou o mundo da programação pede diversificação. Talvez não 10.000 linguagens, mas mais de 1 com certeza. Por isso não me limito ao front-end e venho desenvolvendo minhas habilidades em back-end: no processamento de informações recebidos de usuários e no gerenciamento e armazenamento de dados. Também não é possível ignorar a presença da inteligência artificial e seu estudo vai se tornar cada vez mais relevante. Por fim, preciso completar a trindade (ou tri-force, para minha comunidade gamer) e explorar o caminho da programação mobile.",
 }
+
+export const CertificationContent = [
+  {
+    title: "Pós Graduação em Gestão de TI",
+    image: "/certifications/Diploma Uninter.pdf",
+  },
+  {
+    title: "Graduação em Análise e Desenvolvimento de Sistemas",
+    image: null,
+  },
+  {
+    title: "Certificação no Curso CC50",
+    image: "/public/certifications/Certificado CC50 - Pedro Costa.pdf",
+  },
+  {
+    title: "Certificação em HTML e CSS",
+    image: "/public/certifications/Certificado HTML5 e CSS3.jpg",
+  },
+  {
+    title: "Certificação em Javascript",
+    image: "/public/certifications/Certificado Javascript.jpg",
+  },
+  {
+    title: "Certificação em Bootstrap",
+    image: "/public/certifications/Certificado bootstrap.jpg",
+  },
+  {
+    title: "Certificação em Vue.js",
+    image: "/public/certifications/Certificado Vuejs.jpg",
+  },
+  {
+    title: "Certificação em React",
+    image: null,
+  },
+  {
+    title: "Certificação em Git",
+    image: "/public/certifications/Certificado Git-Github.jpg",
+  },
+  {
+    title: "Certificação em Docker",
+    image: "/public/certifications/Certificado Docker.png",
+  },
+]
--- src/pages/Certification.tsx
+++ src/pages/Certification.tsx
@@ -1,9 +1,60 @@
 import PageTransition from "../components/PageTransition"
+import ScrollArrow from "../components/ScrollArrow"
+import { CertificationContent } from "../content"
 
 const Certification = () => {
   return (
     <PageTransition>
-        <div>Certificação</div>
+      <div className="w-full mx-auto max-w-4xl flex flex-col gap-8">
+        
+        {/* Lista de Certificados */}
+        <ul className="flex flex-col gap-4">
+          {CertificationContent.map((cert, index) => (
+            <li
+              key={index}
+              className="
+                flex flex-col justify-center sm:flex-row
+                sm:items-center sm:justify-between
+                gap-4
+                bg-bg-secondary
+                rounded-xl
+                p-4
+              "
+            >
+              {/* Título */}
+              <h3 className="text-lg font-semibold">
+                {cert.title}
+              </h3>
+
+              {/* Imagem/Link */}
+              {cert.image ? (
+                <a
+                  href={cert.image}
+                  target="_blank"
+                  className="
+                    text-sm
+                    px-4 py-2
+                    rounded-lg
+                    bg-accent-primary
+                    text-bg-primary
+                    hover:opacity-90
+                    transition sm:self-auto
+                  "
+                >
+                  Ver certificado
+                </a>
+              ) : (
+                <span className="text-sm text-text-secondary italic">
+                  Em andamento
+                </span>
+              )}
+            </li>
+          ))}
+        </ul>
+
+        {/* Seya */}
+        <ScrollArrow to="/sobre" label="Sobre mim" />
+      </div>
     </PageTransition>
   )
 }
