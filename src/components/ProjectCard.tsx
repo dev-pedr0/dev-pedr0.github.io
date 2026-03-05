6cd6450a3fbe4faa08f85300f717036fcc0cdf85
Pedro Costa <pedro4costa@outlook.com>
2026-03-05 19:26:38 -0300

    remove: remove mouse interaction effects from ProjectCard component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,3 @@
-import { useRef, useState } from "react"
 import type { Project } from "../content"
 
 type Props = {
@@ -6,41 +5,8 @@ type Props = {
 }
 
 const ProjectCard = ({ project }: Props) => {
-    const cardRef = useRef<HTMLDivElement>(null);
-    const [transform, setTransform] = useState("");
-
-    function handleMouseMove(e: React.MouseEvent<HTMLDivElement>) {
-        const card = cardRef.current
-        if (!card) return
-
-        const rect = card.getBoundingClientRect()
-        const x = e.clientX - rect.left
-        const y = e.clientY - rect.top
-
-        const centerX = rect.width / 2
-        const centerY = rect.height / 2
-
-        const rotateX = -(y - centerY) / 15
-        const rotateY = (x - centerX) / 15
-
-        setTransform(`
-        perspective(800px)
-        rotateX(${rotateX}deg)
-        rotateY(${rotateY}deg)
-        scale(1.03)
-        `)
-    };
-
-    function handleMouseLeave() {
-        setTransform("perspective(800px) rotateX(0) rotateY(0) scale(1)")
-    }
-
     return (
         <article
-            ref={cardRef}
-            onMouseMove={handleMouseMove}
-            onMouseLeave={handleMouseLeave}
-            style={{ transform }}
             className="
                 bg-bg-secondary
                 rounded-xl
